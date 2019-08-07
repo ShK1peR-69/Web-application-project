bd703472e35d30535433ec4699d0fd9f3c0f9dc0
ShK1peR-69 <grigorich.poker@yandex.ru>
2019-08-07 14:21:43 +0300

    Changed header animation settings

--- src/main/webapp/WEB-INF/views/templates/header-hr.ftl
+++ src/main/webapp/WEB-INF/views/templates/header-hr.ftl
@@ -1,6 +1,6 @@
 <div class="main-hr">
-    <img src="/resources/images/fonbet.png" alt="fonbet" id="animate-advert">
-    <img src="/resources/images/fonbet.png" alt="fonbet" id="animate-advert">
-    <img src="/resources/images/fonbet.png" alt="fonbet" id="animate-advert">
+    <img src="/resources/images/fonbet.png" alt="fonbet" id="animate-advert-1">
+    <img src="/resources/images/fonbet.png" alt="fonbet" id="animate-advert-2">
+    <img src="/resources/images/fonbet.png" alt="fonbet" id="animate-advert-3">
 </div>
 <div class="hr-div"></div>
--- src/main/webapp/resources/js/animation.js
+++ src/main/webapp/resources/js/animation.js
@@ -1,5 +1,34 @@
+const TIME_HIDE = 4000;
+const TIME_SHOW = 2700;
 $(document).ready(function () {
-    $("#animate-advert").hide(500);
-    $("#animate-advert").show(500);
+    anim();
 });
 
+function anim() {
+    let block_1 = $("#animate-advert-1");
+    let block_2 = $("#animate-advert-2");
+    let block_3 = $("#animate-advert-3");
+    block_1.animate({
+        opacity: 0
+    }, TIME_HIDE, function () {
+        block_1.animate({
+            opacity: 1
+        }, TIME_SHOW);
+    });
+
+    block_2.animate({
+        opacity: 0
+    }, TIME_HIDE, function () {
+        block_2.animate({
+            opacity: 1
+        }, TIME_SHOW);
+    });
+    block_3.animate({
+        opacity: 0
+    }, TIME_HIDE, function () {
+        block_3.animate({
+            opacity: 1
+        }, TIME_SHOW);
+    });
+    return setTimeout(anim, 8000);
+}
